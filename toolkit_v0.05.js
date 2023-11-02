b7d7af2da5fd776d9dab60433f5e905d864b08b9
GreenFlux <support@greenflux.us>
2023-11-02 11:35:29 -0400

    Update and rename toolkit_v0.04.js to toolkit_v0.05.js

---
+++ toolkit_v0.05.js
@@ -0,0 +1,30 @@
+// Import Lodash as an ES6 module from a CDN
+import _ from 'https://cdn.skypack.dev/lodash-es';
+
+// Define your functions and variables
+function selectOptions(data, label, value) {
+  return _.orderBy(
+    _.uniqBy(data.map(obj => ({
+      label: _.get(obj, label),
+      value: _.get(obj, value)
+    })).filter(obj => !!obj.label && !!obj.value), o => o.label), 'label');
+}
+
+// New function that uses Lodash
+function myFunctionWithLodash(arr) {
+  // Example Lodash operation - Sort the array
+  return _.sortBy(arr);
+}
+
+const myVariable1 = 'Value 1';
+const myVariable2 = 42;
+
+// Export the functions and variables as a toolkit object
+const toolkit = {
+  selectOptions,
+  myFunctionWithLodash, // Add the new function
+  myVariable1,
+  myVariable2
+};
+
+export { toolkit };
